d1981fab81e77c36b1ba693197233ff1ffbeb7df
Geordie King <geordie.king@gmail.com>
2021-07-15 23:55:53 +0100

    End of Lesson 6 videos

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart1: 0,
+      cart2: 0,
       prodName: 'Socks',
       prodDesc: 'Thing to keep your feet warm',
       prodImg: './assets/images/socks_green.jpg',
@@ -11,10 +13,21 @@ const app = Vue.createApp({
       prodOnSale: false,
       prodDetails: ['50% cotton', '30% wool', '20% polyester'],
       prodVariants: [
-        { id: 2234, colour: 'green'},
-        { id: 2235, colour: 'blue'}
+        { id: 2234, colour: 'green', imgSrc: './assets/images/socks_green.jpg'},
+        { id: 2235, colour: 'blue', imgSrc: './assets/images/socks_blue.jpg'}
       ],
       prodSizes: ['S','M','L','XL']
+    }
+  },
+  methods: {
+    // btnAddToCart(){
+    //   this.cart2 += 2
+    // }
+    btnAddToCart() { this.cart2 += 2 },
+    updateImg(imgSrc) { this.prodImg = imgSrc },
+    btnResetCart() { 
+      this.cart1 = 0
+      this.cart2 = 0
     }
   }
 });
