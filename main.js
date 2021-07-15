887584f1927528f8f8ee4e41d78fd61bfd9d7b0f
Geordie King <geordie.king@gmail.com>
2021-07-15 23:39:44 +0100

    Enod of Lesson 5 videos

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ const app = Vue.createApp({
       prodInStock: true,
       prodOOS: false,
       prodInv: 100,
-      prodOnSale: false
+      prodOnSale: false,
+      prodDetails: ['50% cotton', '30% wool', '20% polyester'],
+      prodVariants: [
+        { id: 2234, colour: 'green'},
+        { id: 2235, colour: 'blue'}
+      ],
+      prodSizes: ['S','M','L','XL']
     }
   }
 });
